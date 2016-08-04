2f97819bc02fe9bfe7451f893cddb537c6f07486
sammic432 <sammic432@gmail.com>
2016-08-04 13:38:48 -0700

    UPDATED!!
    i got the girl to move left &right and got it to pause by pushing 'p',
    but there's no button or a way to unpause it

--- python/glassceiling.py
+++ python/glassceiling.py
@@ -1,6 +1,8 @@
-import pygame
+
+import pygame, math, itertools
 import random
 from pygame.locals import*
+import os
 
 
 # Define some colors
@@ -15,157 +17,145 @@ colors = [BLACK, GREEN, BLUE, RED]
 def random_color():
   return random.choice(colors)
 
+def magnitude(v): 
+    return math.sqrt(sum(v[i]*v[i] for i in range(len(v))))
+
+def sub(u, v):
+    return [u[i]-v[i] for i in range(len(u))]
+
+def normalize(v): 
+    return [v[i]/magnitude(v)  for i in range(len(v))]
+
+
 pygame.init()
+
+path = itertools.cycle([(26, 43), (105, 110), (45, 225), (145, 295), (266, 211), (178, 134), (250, 56), (147, 12)])
+target = next(path)
+ball, speed = pygame.rect.Rect(target[0], target[1], 10, 10), 3.6
+pause_text = pygame.font.SysFont('Consolas', 32).render('Pause', True, pygame.color.Color('White'))
+
+pause = False
+unpause = True
+
+RUNNING, PAUSE = 0, 1
+state = RUNNING
  
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 class woman():
-  def __init__(self, screen, x_postition, y_position):
-    self.screen = screen
-    self.x_postition = x_postition
-    self.y_position = y_position
+    def __init__(self, screen, speed, x_position, y_position):
+        self.screen = screen
+        self.speed = speed
+        self.x_position = x_position
+        self.y_position = y_position
+        self.jumpheight = 0
 
-  def create_woman(self):
-    woman = pygame.image.load ("woman-still.png")
-    woman = pygame.transform.scale(woman, (80, 35))
-    self.x_position = random.randint (0, 800)
-    self.y_position = random.randint (0, 100)
-    screen.blit(woman, (self.x_position, self.y_position))
+    def create_woman(self):
+        woman = pygame.image.load ("woman-still.png")
+        woman = pygame.transform.scale(woman, (350, 250))
+        screen.blit(woman, (self.x_position, self.y_position))
 
-  def move_woman (self, speed):
-    self.x_position += speed
+    def move_woman (self, speed):
+        self.x_position += self.speed
 
-class Building():
-  def __init__(self, x_point, y_point, width, height, color):
-    self.x_point= x_point
-    self.y_point= y_point
-    self.width= width
-    self.height= height
-    self.color= color
+        if self.jumpheight > 0: self.jumpheight -= 1
+        feet_y = SCREEN_HEIGHT - self.jumpheight
+        if self.speed == 0:
+            # woman1- still
+            print ("still woman")
 
-  def draw(self):
-    pygame.draw.rect(screen, self.color, (self.x_point, SCREEN_HEIGHT - self.height, self.width, self.height))
+        elif self.runstage < 20:
+            # woman2
+            print ("woman 2")
+            self.runstage += self.speed
 
-  def move(self, speed):
-    self.x_point -= speed
+        elif self.runstage < 40:
+            # woman3
+            print ("woman 3")
+            self.runstage += self.speed
+        elif self.runstage >= 40: self.runstage = 1
 
-class Scroller(object):
-  
-  def __init__(self, width, height, base, color, speed):
-    self.width= width
-    self.height= height
-    self.base= base
-    self.color= color
-    self.speed= speed
-    self.building_list= []
-    self.add_buildings()
-    
-  def add_buildings(self):
-    self.add_building(-120,120)
-    random_width = 0
-    x_location=0
-    while x_location<= self.width:
-      random_width = random.randint(40, 180)
-      self.add_building(x_location, random_width)
-      x_location = x_location + random_width  + 20
- 
-  def add_building(self, x_location, combined_width):
-    building1 = Building(x_location, self.base, combined_width, random.randint(80, 200), random_color())
-    self.building_list.append(building1)
+    def right(self):
+        self.x_position = self.x_position + 15
 
-  def draw_buildings(self):
-    pygame.draw.rect(screen, self.color, (0, SCREEN_HEIGHT, self.width, SCREEN_HEIGHT - self.height))
-    for building1 in self.building_list:
-      building1.draw()
+    def left(self):
+        self.x_position = self.x_position - 15
 
-  def move_buildings(self, runningspeed):
-    for building1 in self.building_list:
-      building1.move(self.speed + runningspeed)
-      last_building = self.building_list[-1]
-      if last_building.x_point > self.width:
-        self.building_list.remove(last_building) 
-      first_building = self.building_list[0]
-      if first_building.x_point > 20:
-        building_width = random.randint(40,180)
-        b = Building(-building_width, 0, building_width, random.randint(80, 400), random_color())
-        self.building_list.insert(0,b)
+    def jump(self):
+        # if self.jumpheight < SCREEN_HEIGHT - 100: self.jumpheight += 10
+        self.y_position -= 15
 
-class RunningPerson():
-  def __init__(self, x_point, color, speed):
-    self.x_point= x_point
-    self.color= color
-    self.runstage = 1
-    self.speed = speed
-    self.jumpheight = 0
+        self.y_position +=15
 
-  def draw(self):
-    if self.jumpheight > 0: self.jumpheight -= 1
-    feet_y = SCREEN_HEIGHT - self.jumpheight
-    if self.speed == 0:
-      # woman1- still
+class button():
+    def __init__(self, text, position, size, font, color):
+        self.text = text
+        self.position = position
+        self.size = size
+        self.font = font
+        self.color = color
+    def paused():
+      largeText = pygame.font.SysFont("comicsansms",115)
+      TextSurf, TextRect = text_objects("Paused", largeText)
+      TextRect.center = ((display_width/2),(display_height/2))
+      gameDisplay.blit(TextSurf, TextRect)
 
-    elif self.runstage < 20:
-      # woman2
-      self.runstage += self.speed
-
-    elif self.runstage < 40:
-      # woman3
-      self.runstage += self.speed
-    if self.runstage >= 40: self.runstage = 1
-
-  def faster(self):
-        if self.speed < 10:
-            self.speed +=1
-
-  def slower(self):
-    if self.speed > 0:
-        self.speed -= 1
-
-  def jump(self):
-      if self.jumpheight < SCREEN_HEIGHT - 100: self.jumpheight += 20
 
     
-FRONT_SCROLLER_COLOR = (0,0,30)
-MIDDLE_SCROLLER_COLOR = (30,30,100)
-BACK_SCROLLER_COLOR = (50,50,150)
-BACKGROUND_COLOR = (135,206,235)
+BACKGROUND_PICTURE = pygame.image.load("office.jpg")
+BACKGROUND_PICTURE2 = pygame.image.load("office.jpg")
+BACKGROUND_PICTURE_x = 0
+BACKGROUND_PICTURE2_x = BACKGROUND_PICTURE.get_width()
  
-front_scroller = Scroller(SCREEN_WIDTH, 500, 0, FRONT_SCROLLER_COLOR, 3)
-middle_scroller = Scroller(SCREEN_WIDTH, 300, 100, MIDDLE_SCROLLER_COLOR, 2)
-back_scroller = Scroller(SCREEN_WIDTH, 400, 200, BACK_SCROLLER_COLOR, 1)
-runner = RunningPerson(int(SCREEN_WIDTH/2), WHITE, 0)
+runner = woman(screen, 3, 80, 350)
+
 
 pygame.display.set_caption("CityScroller") 
 clock = pygame.time.Clock() 
 done = False
+
 while not done:
-   for event in pygame.event.get():
+
+    for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-               runner.faster()
+                runner.right()
             elif event.key == pygame.K_LEFT:
-                runner.slower()
+                runner.left()
             elif event.key == pygame.K_SPACE:
                 runner.jump()
+            if event.key == pygame.K_p: 
+                state = PAUSE
+            if event.key == pygame.K_s: 
+                state = RUNNING
         if event.type == pygame.QUIT:
             done = True
- 
-   screen.fill(BACKGROUND_COLOR)
- 
-   back_scroller.draw_buildings()
-   middle_scroller.draw_buildings()
-   front_scroller.draw_buildings()
-   runner.draw()
 
-   if runner.speed > 0:
-     back_scroller.move_buildings(runner.speed)
-     middle_scroller.move_buildings(runner.speed)
-     front_scroller.move_buildings(runner.speed)
+        if state == RUNNING:
+          target_vector = sub(target, ball.center) 
 
-   pygame.display.flip()
+        if magnitude(target_vector) < 2: 
+          target = next(path)
+        else:
+          ball.move_ip([c * speed for c in normalize(target_vector)])
+
+          pygame.draw.rect(screen, pygame.color.Color('Yellow'), ball)
+
+        if state == PAUSE:
+          screen.blit(pause_text, (100, 100))
+          os.system("pause")
+
+    screen.blit(BACKGROUND_PICTURE, (0,50))
+
+    runner.create_woman()
+
+    button("Continue", (300, 350), 36, "comicsansms", GREEN)
+    button("Quit", (500, 350), 36, "comicsansms", RED)
+
+    pygame.display.flip()
  
-   clock.tick(60)
+    clock.tick(60)
 
 pygame.quit()
