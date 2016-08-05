e5409ff8404f1f12ed21e73186bb504b28f0c036
ZhuLiya <k1781927345@gmail.com>
2016-08-05 09:44:00 -0700

    got the button work

--- python/glassceiling.py
+++ python/glassceiling.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-import pygame, Buttons
+import Buttons
 from pygame.locals import*
 
 
@@ -30,6 +30,50 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 woman_detail= pygame.image.load('woman-still.png').convert_alpha()
 
 l1=pygame.font.SysFont("comicsansms",20)
+
+class Button:
+    def __init__(self, nothing):
+      self.nothing=nothing
+    def create_button(self, surface, color, x, y, length, height, width, text, text_color):
+        surface = self.draw_button(surface, color, length, height, x, y, width)
+        surface = self.write_text(surface, text, text_color, length, height, x, y)
+        self.rect = pygame.Rect(x,y, length, height)
+        return surface
+
+    def write_text(self, surface, text, text_color, length, height, x, y):
+        font_size = int(length//len(text))
+        myFont = pygame.font.SysFont("Calibri", font_size)
+        myText = myFont.render(text, 1, text_color)
+        surface.blit(myText, ((x+length/2) - myText.get_width()/2, (y+height/2) - myText.get_height()/2))
+        return surface
+
+    def draw_button(self, surface, color, length, height, x, y, width):           
+        for i in range(1,10):
+            s = pygame.Surface((length+(i*2),height+(i*2)))
+            s.fill(color)
+            alpha = (255/(i+2))
+            if alpha <= 0:
+                alpha = 1
+            s.set_alpha(alpha)
+            pygame.draw.rect(s, color, (x-i,y-i,length+i,height+i), width)
+            surface.blit(s, (x-i,y-i))
+        pygame.draw.rect(surface, color, (x,y,length,height), 0)
+        pygame.draw.rect(surface, (190,190,190), (x,y,length,height), 1)  
+        return surface
+
+    def pressed(self, mouse):
+        if mouse[0] > self.rect.topleft[0]:
+            if mouse[1] > self.rect.topleft[1]:
+                if mouse[0] < self.rect.bottomright[0]:
+                    if mouse[1] < self.rect.bottomright[1]:
+                        print ("Some button was pressed!")
+                        return True
+                    else: return False
+                else: return False
+            else: return False
+        else: return False
+
+
 
 class woman():
   def __init__(self, screen, x_position, y_position):
@@ -67,7 +111,8 @@ BACKGROUND_PICTURE = pygame.image.load("office.jpg")
 BACKGROUND_PICTURE2 = pygame.image.load("office.jpg")
 BACKGROUND_PICTURE_x = 0
 BACKGROUND_PICTURE2_x = BACKGROUND_PICTURE.get_width()
- 
+Button1=Button(832)
+
 # runner = woman(screen, 80, 350)
 
 runner = woman(screen, 0, 200)
@@ -100,8 +145,14 @@ while not done:
     runner.create_woman()
     runner.show_question()
 
-    # if __name__ == '__main__':
-    #   obj = Button_Example()
+    Button1.create_button(screen, (107,142,35), 225, 135, 200,    100,    0,        "Example", (255,255,255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        elif event.type == MOUSEBUTTONDOWN:
+            if Button1.pressed(pygame.mouse.get_pos()):
+                print ("Give me a command!")
+
 
     pygame.display.flip()
  
